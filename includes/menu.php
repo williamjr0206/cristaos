8ad8eb44f5da252e7ddaf94292e6afae63a059f0
williamjr0206 <williamjr@milbr.net>
2026-03-03 11:45:21 -0300

    Atualizando path do menu.php.

--- includes/menu.php
+++ includes/menu.php
@@ -1,5 +1,5 @@
 <nav style="margin-bottom: 20px;">
-    <a href="<?= BASE_URL ?>index.php">🏠 Início</a> |
+    <a href="<?= BASE_URL ?>/index.php">🏠 Início</a> |
     <a href="<?= BASE_URL ?>/cadastros/usuarios.php">👤 Usuários</a> |
     <a href="<?= BASE_URL ?>logout.php">🚪 Sair</a>
 </nav>
